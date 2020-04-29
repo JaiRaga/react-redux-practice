6061e1d9fa29a983458c1ce0c280b28492dd8a4c
JaiRaga <raga.j.santhosh@gmail.com>
2020-04-29 20:53:35 +0530

    connected with redux store using provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+import store from "./store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
+import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
+import NumberCakes from "./components/NumberCakes";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <ItemContainer cake />
+        <ItemContainer />
+
+        <CakeContainer />
+        <HooksCakeContainer />
+
+        <IceCreamContainer />
+        <HooksIceCreamContainer />
+
+        <NumberCakes />
+      </div>
+    </Provider>
   );
 }
 
